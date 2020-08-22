29cfae29baa1fc23f43d8ec3ea76d95463959417
buiminhquan <wn1409x3@gmail.com>
2020-08-22 11:03:40 +0700

    Add ThanhToan form, DTO, DAO.

---
+++ QuanLyBanHangQuaMang/DTO/ThanhToanDTO.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanHangQuaMang.DTO
+{
+    class ThanhToanDTO
+    {
+        private int _maHoaDon;
+        private string _loaiThanhToan;
+        private double _soTienNhan;
+        private string _soTaiKhoan;
+
+        public int maHoaDon
+        {
+            get { return _maHoaDon; }
+            set { _maHoaDon = value; }
+        }
+
+        public string loaiThanhToan
+        {
+            get { return _loaiThanhToan; }
+            set { _loaiThanhToan = value; }
+        }
+
+        public double soTienNhan
+        {
+            get { return _soTienNhan; }
+            set { _soTienNhan = value; }
+        }
+
+        public string soTaiKhoan
+        {
+            get { return _soTaiKhoan; }
+            set { _soTaiKhoan = value; }
+        }
+
+        public ThanhToanDTO()
+        {
+            _maHoaDon = -1;
+            _loaiThanhToan = "";
+            _soTienNhan = 0;
+            _soTaiKhoan = "";
+        }
+
+        public ThanhToanDTO(int maHoaDon, string loaiThanhToan, double soTienNhan, string soTaiKhoan)
+        {
+            _maHoaDon = maHoaDon;
+            _loaiThanhToan = loaiThanhToan;
+            _soTienNhan = soTienNhan;
+            _soTaiKhoan = soTaiKhoan;
+        }
+    }
+}
